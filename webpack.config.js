e8d990ebc33b9f234d1d93a0edda8c640a51081c
Aoko <hejunqin233@gmail.com>
2019-08-09 16:33:30 +0800

    (refactor): package 调整

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
 
 module.exports = {
   entry: "./dist/index.js",
@@ -8,6 +7,5 @@ module.exports = {
   output: {
     filename: "mahjong-helper.min.js",
     path: path.resolve(__dirname, "min")
-  },
-  plugins: [new LodashModuleReplacementPlugin()]
+  }
 };
